74197af42455bb6e652ac9919b67029021a8a5c9
LWL <766649508@qq.com>
2024-11-19 11:45:44 +0800

    feat：增加适配

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./styles/index.css";
+import "lib-flexible";
 
 createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(process.cwd(), "src")
+    },
+  },
   plugins: [vue()],
 })
